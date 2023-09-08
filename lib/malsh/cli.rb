81905baf02d62687c8c7b6c4e7b4801efee9be1e
pyama <pyama@pepabo.com>
2023-09-08 16:11:21 +0900

    ignore kubernetes

--- lib/malsh/cli.rb
+++ lib/malsh/cli.rb
@@ -1,17 +1,16 @@
 require 'malsh'
 require 'parallel'
-require 'pp'
 module Malsh
   class CLI < Thor
-    class_option :slack_webhook, :type => :string, :aliases => :sw
-    class_option :slack_channel, :type => :string, :aliases => :sc
-    class_option :slack_user, :type => :string, :aliases => :su
-    class_option :api_key, :type => :string, :aliases => :k
-    class_option :subject, :type => :string, :aliases => :s
-    class_option :org, :type => :string, :aliases => :o
-    class_option :invert_match, :type => :array, :aliases => :v
-    class_option :invert_role, :type => :array, :aliases => :r
-    class_option :regexp,:type => :array, :aliases => :e
+    class_option :slack_webhook, type: :string, aliases: :sw
+    class_option :slack_channel, type: :string, aliases: :sc
+    class_option :slack_user, type: :string, aliases: :su
+    class_option :api_key, type: :string, aliases: :k
+    class_option :subject, type: :string, aliases: :s
+    class_option :org, type: :string, aliases: :o
+    class_option :invert_match, type: :array, aliases: :v
+    class_option :invert_role, type: :array, aliases: :r
+    class_option :regexp, type: :array, aliases: :e
 
     desc 'retire', 'check forget retire host'
     def retire
@@ -21,12 +20,13 @@ module Malsh
         m = Malsh.host_metrics(
           h.id,
           Malsh.host_metric_names(h.id).first,
-          Time.now.to_i - 86400,
+          Time.now.to_i - 86_400,
           Time.now.to_i
         )
-        h if (!m || (m["metrics"] && m["metrics"].size == 0)) && !(h["meta"].has_key?("cloud") && %w(elb rds cloudfront).include?(h["meta"]["cloud"]["provider"]))
+        h if (!m || (m['metrics'] && m['metrics'].size == 0)) &&
+             !(h['meta'].has_key?('cloud') && %w[elb rds cloudfront kubernetes].include?(h['meta']['cloud']['provider']))
       end.flatten.compact
-      Malsh.notify_host("退役未了ホスト一覧", hosts)
+      Malsh.notify_host('退役未了ホスト一覧', hosts)
     end
 
     desc 'maverick', 'check no role'
@@ -37,22 +37,22 @@ module Malsh
         h if h.roles.keys.size < 1
       end.flatten.compact
 
-      Malsh.notify_host("ロール無所属ホスト一覧", hosts)
+      Malsh.notify_host('ロール無所属ホスト一覧', hosts)
     end
 
     desc 'search', 'search hosts'
-    option :past_date , :type => :numeric, :aliases => :p
-    option :cpu_threshold, :type => :numeric, :aliases => :c
-    option :memory_threshold, :type => :numeric, :aliases => :m
-    option :status, :type => :string, :aliases => :st
+    option :past_date, type: :numeric, aliases: :p
+    option :cpu_threshold, type: :numeric, aliases: :c
+    option :memory_threshold, type: :numeric, aliases: :m
+    option :status, type: :string, aliases: :st
     def search
       Malsh.init options
       o = options[:status] ? { status: options[:status] } : {}
       hosts = Malsh.hosts(o)
-      Object.const_get("Malsh::HostMetrics").constants.each do |c|
+      Object.const_get('Malsh::HostMetrics').constants.each do |c|
         hosts = Object.const_get("Malsh::HostMetrics::#{c}").check(hosts)
       end
-      Malsh.notify_host("ホスト一覧", hosts.compact)
+      Malsh.notify_host('ホスト一覧', hosts.compact)
     end
 
     desc 'alert', 'list alerts'
@@ -63,7 +63,7 @@ module Malsh
     end
 
     map %w[--version -v] => :__print_version
-    desc "--version, -v", "print the version"
+    desc '--version, -v', 'print the version'
     def __print_version
       puts Malsh::VERSION
     end
